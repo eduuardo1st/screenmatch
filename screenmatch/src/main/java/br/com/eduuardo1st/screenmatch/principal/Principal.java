8b6851919c741557f61dbb07d78c4dbd37dfc08f
Eduardo Gonçalves <eduardouc388@gmail.com>
2026-02-28 21:36:13 -0300

    Update

--- screenmatch/src/main/java/br/com/eduuardo1st/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/eduuardo1st/screenmatch/principal/Principal.java
@@ -1,13 +1,17 @@
 package br.com.eduuardo1st.screenmatch.principal;
 
+import br.com.eduuardo1st.screenmatch.model.DadosEpisodio;
 import br.com.eduuardo1st.screenmatch.model.DadosSerie;
 import br.com.eduuardo1st.screenmatch.model.DadosTemporada;
+import br.com.eduuardo1st.screenmatch.model.Episodio;
 import br.com.eduuardo1st.screenmatch.service.ConsumoApi;
 import br.com.eduuardo1st.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -37,5 +41,30 @@ public class Principal {
 
         temporadas.forEach(System.out::println);
 
+//        for(int i = 0; i < dados.totalTemporadas(); i++){
+//            List<DadosEpisodio> episodiosTemporadas = temporadas.get(i).episodios();
+//            for(int j = 0; j < episodiosTemporadas.size(); j++){
+//                System.out.println(episodiosTemporadas.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                        .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                        .limit(5)
+                        .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                .map(d -> new Episodio(t.numero(), d)))
+                .collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
     }
 }
